b84550641aa08d4df2c8116dce9a12820ecd7d5c
Vaibhav Kumar Thakur <vaibhavkr522@gmail.com>
2025-04-18 10:18:15 +0530

    project-card

--- src/pages/StudentProjectsPage.jsx
+++ src/pages/StudentProjectsPage.jsx
@@ -12,7 +12,7 @@ const StudentProjectsPage = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           <ProjectCard image="src/assets/Rajesh.jpg" />
           <ProjectCard image="src/assets/Vignesh.jpg" />
-          <ProjectCard image="src\assets\Sreesankar.jpg" />
+          <ProjectCard image="src/assets/Sreesankar.jpg" />
         </div>
       </div>
     </div>
